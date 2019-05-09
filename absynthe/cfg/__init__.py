ffe01a854f1a661ab814b0d13dd943452517d59d
N. Chaturv3di <chaturv3di@users.noreply.github.com>
2019-05-09 14:51:49 +0530

    Added Graph class and unit tests.

--- absynthe/cfg/__init__.py
+++ absynthe/cfg/__init__.py
@@ -1,3 +1,4 @@
 from __future__ import absolute_import
 
 from .node import Node, UniformNode
+from .graph import Graph
---
+++ test/test_Graph.py
@@ -0,0 +1,55 @@
+import unittest
+
+from absynthe.cfg.graph import Graph
+from absynthe.cfg.node import UniformNode
+
+
+class GraphTest(unittest.TestCase):
+
+    def test_basicErrors(self):
+        graph = Graph("EmptyGraph", 0)
+        uselessNode = UniformNode("UselessNode")
+
+        # Node cannot be added since 'graph' is created
+        # with numRoots = 0
+        with self.assertRaises(IndexError):
+            graph.addRoot(uselessNode)
+        return
+
+    def test_graphSizeZero(self):
+        graph = Graph("EmptyGraph", 0)
+        self.assertEqual(graph.size(), 0)
+        return
+
+    def test_graphSizeNonZero(self):
+        root1 = UniformNode("Root1")
+        nodeR1S1 = UniformNode("Root1_Succ1")
+        nodeR1S2 = UniformNode("Root1_Succ2")
+        root1.addSuccessor(nodeR1S1)
+        root1.addSuccessor(nodeR1S2)
+
+        # Add successors to nodeR1S1
+        nodeS1S1 = UniformNode("Succ1_Succ1")
+        nodeS1S2 = UniformNode("Succ1_Succ2")
+        nodeR1S1.addSuccessor(nodeS1S1)
+        nodeR1S1.addSuccessor(nodeS1S2)
+
+        root2 = UniformNode("Root2")
+        nodeR2S1 = UniformNode("Root2_Succ1")
+        nodeR2S2 = UniformNode("Root2_Succ2")
+        root2.addSuccessor(nodeR2S1)
+        root2.addSuccessor(nodeR2S2)
+
+        # Create a cycle with edge nodeR2S1 -> root1
+        nodeR2S1.addSuccessor(root1)
+
+        graph = Graph("TestGraph", 2)
+        graph.addRoot(root1)
+        graph.addRoot(root2)
+
+        self.assertEqual(graph.size(), 8)
+        return
+
+
+if __name__ == '__main__':
+    unittest.main()
