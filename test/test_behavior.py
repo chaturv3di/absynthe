3b4dfbc08735a7ccf2d85f810a3c0fbe4127a599
N. Chaturv3di <chaturv3di@users.noreply.github.com>
2019-05-29 23:29:55 +0530

    Cleaned up test output

--- test/test_behavior.py
+++ test/test_behavior.py
@@ -1,24 +1,30 @@
 import unittest
+import os
 
 from absynthe.graph_builder import TreeBuilder
 from absynthe.behaviors import SimpleBehavior
 
 
 class test_simpleBehavior(unittest.TestCase):
-    tree_kwargs = {TreeBuilder.KW_NUM_ROOTS: "2",
-                   TreeBuilder.KW_NUM_LEAVES: "4",
-                   TreeBuilder.KW_BRANCHING_DEGREE: "2",
-                   TreeBuilder.KW_MIN_NUM_INNER_NODES: "8",
-                   TreeBuilder.KW_MAX_NUM_INNER_NODES: "16",
-                   TreeBuilder.KW_SUPPORTED_NODE_TYPES: "SimpleLoggerNode"}
 
-    simpleTreeBuilder = TreeBuilder(**tree_kwargs)
+    def test_basicLogGeneration(self):
+        tree_kwargs = {TreeBuilder.KW_NUM_ROOTS: "2",
+                       TreeBuilder.KW_NUM_LEAVES: "4",
+                       TreeBuilder.KW_BRANCHING_DEGREE: "2",
+                       TreeBuilder.KW_MIN_NUM_INNER_NODES: "8",
+                       TreeBuilder.KW_MAX_NUM_INNER_NODES: "16",
+                       TreeBuilder.KW_SUPPORTED_NODE_TYPES: "SimpleLoggerNode"}
 
-    testBehavior = SimpleBehavior()
-    testBehavior.addGraph(simpleTreeBuilder.generateNewGraph())
-    testBehavior.addGraph(simpleTreeBuilder.generateNewGraph())
-    testBehavior.addGraph(simpleTreeBuilder.generateNewGraph())
-    testBehavior.addGraph(simpleTreeBuilder.generateNewGraph())
+        simpleTreeBuilder = TreeBuilder(**tree_kwargs)
 
-    for logLine in testBehavior.synthesize(2):
-        print(logLine)
+        testBehavior = SimpleBehavior()
+        testBehavior.addGraph(simpleTreeBuilder.generateNewGraph())
+        testBehavior.addGraph(simpleTreeBuilder.generateNewGraph())
+        testBehavior.addGraph(simpleTreeBuilder.generateNewGraph())
+        testBehavior.addGraph(simpleTreeBuilder.generateNewGraph())
+
+        fileName = os.path.dirname(os.getcwd() + "/test/resources/") + "/SimpleBehavior_test.log"
+        with open(fileName, 'w') as logfile:
+            for logLine in testBehavior.synthesize(2):
+                logfile.write(logLine)
+                logfile.write(os.linesep)
