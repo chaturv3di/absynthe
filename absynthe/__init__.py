cd47b5343dac6f8261dedde122bbed6cab937304
N. Chaturv3di <chaturv3di@users.noreply.github.com>
2019-06-16 22:02:59 +0530

    Updated import

--- absynthe/__init__.py
+++ absynthe/__init__.py
@@ -1,4 +1,4 @@
-# from __future__ import absolute_import
+from __future__ import absolute_import
 
 # from .graph_builder import GraphBuilder, TreeBuilder
 # from .behavior import Behavior, MonospaceInterleaving
